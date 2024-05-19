42d8a7eca17abf93443d8ff6d2ebca2db531bd99
Alisue <lambdalisue@gmail.com>
2024-05-19 14:14:48 +0900

    Use external process to produce dummy stream

--- denops/@fall-extension/dummy.ts
+++ denops/@fall-extension/dummy.ts
@@ -1,7 +1,10 @@
 import type { GetSource } from "https://deno.land/x/fall_core@v0.11.0/mod.ts";
+import { fromFileUrl } from "https://deno.land/std@0.217.0/path/from_file_url.ts";
+import { TextLineStream } from "jsr:@std/streams@0.224.0/text-line-stream";
 import { delay } from "jsr:@std/async/delay";
 import { faker } from "npm:@faker-js/faker";
-import { assert, is } from "jsr:@core/unknownutil@3.18.0";
+import { toJson } from "jsr:@std/streams@0.224.0/to-json";
+import { assert, ensure, is } from "jsr:@core/unknownutil@3.18.0";
 
 const isOptions = is.StrictOf(is.PartialOf(is.ObjectOf({
   count: is.Number,
@@ -12,27 +15,66 @@ const isOptions = is.StrictOf(is.PartialOf(is.ObjectOf({
 export const getSource: GetSource = (_denops, options) => {
   assert(options, isOptions);
   const count = options.count ?? 1_000_000;
-  const intervalCount = options.intervalCount ?? 100;
-  const intervalDelay = options.intervalDelay ?? 10;
+  const intervalCount = options.intervalCount ?? 1000;
+  const intervalDelay = options.intervalDelay ?? 1;
   return {
     stream() {
       return new ReadableStream({
         async start(controller) {
-          for (let index = 0; index < count; index++) {
-            const path = faker.system.filePath();
-            controller.enqueue({
-              value: path,
-              detail: {
-                path,
-                index,
-              },
-            });
-            if (index % intervalCount === 0) {
-              await delay(intervalDelay);
-            }
-          }
+          const command = new Deno.Command(Deno.execPath(), {
+            args: ["run", "-A", fromFileUrl(import.meta.url)],
+            stdin: "piped",
+            stdout: "piped",
+          });
+          const proc = command.spawn();
+          await Promise.all([
+            proc.status,
+            ReadableStream
+              .from(JSON.stringify({ count, intervalCount, intervalDelay }))
+              .pipeThrough(new TextEncoderStream())
+              .pipeTo(proc.stdin),
+            proc.stdout
+              .pipeThrough(new TextDecoderStream())
+              .pipeThrough(new TextLineStream())
+              .pipeTo(
+                new WritableStream({
+                  write(json) {
+                    controller.enqueue(JSON.parse(json));
+                  },
+                }),
+              ),
+          ]);
+          controller.close();
         },
       });
     },
   };
 };
+
+if (import.meta.main) {
+  const { count, intervalCount, intervalDelay } = ensure(
+    await toJson(Deno.stdin.readable),
+    is.ObjectOf({
+      count: is.Number,
+      intervalCount: is.Number,
+      intervalDelay: is.Number,
+    }),
+  );
+  try {
+    for (let index = 0; index < count; index++) {
+      const path = faker.system.filePath();
+      console.log(JSON.stringify({
+        value: path,
+        detail: {
+          path,
+          index,
+        },
+      }));
+      if (index % intervalCount === 0) {
+        await delay(intervalDelay);
+      }
+    }
+  } catch {
+    // Fail silently
+  }
+}
